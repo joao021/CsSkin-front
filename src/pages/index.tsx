c3bbb6e7b8e0c4791ac590b18339f2bab8a16a7b
joao021 <joaoramalho30@yahoo.com.br>
2024-09-09 23:29:07 -0300

    feat: configure Chakra UI

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, Chakra UI!</h1>
+      <Button colorScheme="brand" size="lg">
+        Clique Aqui
+      </Button>
+    </div>
+  );
+}
